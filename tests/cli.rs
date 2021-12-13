daa130a7ae1e10e41de6c431adc7cb1b77dc4c91
Mick van Gelderen <mickvangelderen@gmail.com>
2021-12-13 21:12:02 +0100

    Determine exe path using cargo

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,10 +1,13 @@
+use std::path::Path;
 use std::process::Command;
 
-const CLI: &str = "challenge_watch_cli";
+fn exe_path() -> &'static Path {
+    std::path::Path::new(env!("CARGO_BIN_EXE_challenge_watch_cli"))
+}
 
 #[test]
 fn display_help() {
-    let output = Command::new(CLI)
+    let output = Command::new(exe_path())
         .arg("--help")
         .output()
         .expect("Failed to run command!");
@@ -13,7 +16,12 @@ fn display_help() {
 
 #[test]
 fn no_path() {
-    let output = Command::new(CLI).output().expect("Failed to run command!");
-    assert_eq!(String::from_utf8(output.stderr).expect("stderr is not valid utf-8!"), "error: The following required arguments were not provided:\n    <PATH>...\n\nUSAGE:\n    challenge_watch_cli <PATH>...\n\nFor more information try --help\n");
+    let output = Command::new(exe_path())
+        .output()
+        .expect("Failed to run command!");
+    let stderr = String::from_utf8(output.stderr).expect("stderr is not valid utf-8!");
+    assert!(stderr.starts_with(
+        "error: The following required arguments were not provided:\n    <PATH>...\n"
+    ));
     assert!(!output.status.success());
 }
