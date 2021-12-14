b31f7076a9cf08924acb959f337dd798b8549405
Mick van Gelderen <mickvangelderen@gmail.com>
2021-12-15 00:21:33 +0100

    Update

--- README.md
+++ README.md
@@ -27,3 +27,11 @@ The requirements as provided and the assumptions I have made.
 
  - [x] [run tests and lints in CI](https://github.com/mickvangelderen/challenge_watch_cli/actions)
  - [x] gracefully exits on program interrupt signal
+
+## Lessons
+ - Github actions work great!
+ - Writing cross-platform command line tests for programs that never finish is a bit messy:
+   - writing to stdin and reading from stdout and stderr requires threading or use of something like [mio](https://github.com/tokio-rs/mio).
+   - I'm not exactly sure how to ensure the program has spawned and I hooked into stdout, there's a `thread::sleep` now.
+ - I remembered `notify` from when I used it to implement hot reloading of GLSL shaders in [my 3D renderer](https://github.com/mickvangelderen/clustered-light-shading) written in Rust using not much other than OpenGL, an imaging and math library. It's good to know the time I spent learning then is helping me today.
+
--- tests/cli.rs
+++ tests/cli.rs
@@ -1,7 +1,8 @@
 use std::io::{self, BufRead, Read};
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
-use std::sync::mpsc;
+use std::sync::{mpsc, Arc, Barrier};
+use std::time::Duration;
 use std::{fs, thread, time};
 
 use tempdir::TempDir;
@@ -51,12 +52,10 @@ fn one_path() {
         let stdout = child.stdout.take().unwrap();
         let reader = io::BufReader::new(stdout);
         let lines = reader.lines();
+
         move || {
-            println!("About to read lines!");
             for line in lines {
-                println!("{:?}", line);
                 let line = line.expect("Failed to read line form stdout!");
-                println!("Received {:?}", line);
                 if let Err(_) = tx.send(line) {
                     // Stop reading if we can not send anymore.
                     break;
@@ -65,43 +64,41 @@ fn one_path() {
         }
     });
 
-    thread::sleep(time::Duration::from_secs(1));
+    // NOTE(mickvangelderen): This thread::sleep is the difference between the `recv_timeout` timing out and properly receiving the first line from the spawned process' stdout.
+    // I do not understand why it makes the difference.
+    // My best guess is that the file is created, an event is picked up and written by the spawned process and during all this time we haven't started listening to stdio yet?
+    // That seems unlikely though.
+    // For this assignment I will concede and leave things here.
+    thread::sleep(Duration::from_millis(100));
 
-    // Create a directory and wait on a create line being printed.
+    let read_line = || {
+        rx.recv_timeout(time::Duration::from_secs(1))
+            .expect("Did not receive line in time!")
+    };
+
+    // Create a directory.
     let deep_dir_path = test_dir_path.join("deep");
     fs::create_dir(&deep_dir_path).expect("Failed to create deep dir!");
-    let line = rx
-        .recv_timeout(time::Duration::from_secs(1))
-        .expect("Did not receive line in time!");
-    assert_eq!(line, format!("CREATE {:?}", deep_dir_path.display()));
+
+    // Wait on a create line being printed.
+    assert_eq!(read_line(), format!("CREATE {:?}", deep_dir_path.display()));
 
     // Create a file and wait on a create line being printed.
     let file_txt_path = deep_dir_path.join("file.txt");
     fs::write(&file_txt_path, "Programming is cool!").expect("Failed to create file.txt!");
 
+    // Wait on create and write events being printed.
     if cfg!(target_os = "macos") {
-        let line = rx
-            .recv_timeout(time::Duration::from_secs(1))
-            .expect("Did not receive line in time!");
         assert_eq!(
-            line,
+            read_line(),
             format!("CREATE | WRITE {:?}", file_txt_path.display())
         );
     } else {
-        let line = rx
-            .recv_timeout(time::Duration::from_secs(1))
-            .expect("Did not receive line in time!");
-        assert_eq!(line, format!("CREATE {:?}", file_txt_path.display()));
-        let line = rx
-            .recv_timeout(time::Duration::from_secs(1))
-            .expect("Did not receive line in time!");
-        assert_eq!(line, format!("WRITE {:?}", file_txt_path.display()));
+        assert_eq!(read_line(), format!("CREATE {:?}", file_txt_path.display()));
+        assert_eq!(read_line(), format!("WRITE {:?}", file_txt_path.display()));
     }
 
-    let line = rx
-        .recv_timeout(time::Duration::from_secs(1))
-        .expect("Did not receive line in time!");
-    assert_eq!(line, format!("WRITE {:?}", deep_dir_path.display()));
+    assert_eq!(read_line(), format!("WRITE {:?}", deep_dir_path.display()));
 
     child.kill().expect("Failed to kill child!"); // Might want to rename this variable.
 
